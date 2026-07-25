c483b8f91c4ddab0c8936a4e1034d79a1fa3caeb
unknown <techmikax@gmail.com>
2026-07-25 22:00:57 +0700

    stream layout

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import {
   useSearchParams,
   Link,
   useParams,
+  data,
 } from "react-router-dom";
 
 // Import Swiper React components
@@ -781,7 +782,6 @@ function SeasonList({ seasons, id, type }) {
     </>
   );
 }
-
 function SeasonDetail() {
   const { seasonNumber, id } = useParams();
   const detail = useMovieDetail("tv", id);
@@ -882,7 +882,7 @@ function EpisodeList({ dataSeason }) {
           : nfound;
         return (
           <Link
-            to={`/stream/${episode.id}/${episode.season_number}/${episode.episode_number}`}
+            to={`/stream/${episode.show_id}/${episode.season_number}/${episode.episode_number}`}
             className="episode-card"
             key={episode.episode_number}
           >
@@ -915,7 +915,192 @@ function EpisodeList({ dataSeason }) {
     </div>
   );
 }
+function StreamDetail() {
+  const { id, seasonNumber, episodeNumber } = useParams();
+  const dataSeason = useSeasonDetail(id, seasonNumber);
+  const dataMovie = useMovieDetail("tv", id);
+  if (!dataMovie) return;
+  console.log(dataSeason);
+  console.log(dataMovie);
+  return (
+    <div className="watch-layout">
+      <StreamLayout
+        dataEpisode={dataSeason.episodes[episodeNumber - 1]}
+        dataSeason={dataSeason}
+        dataMovie={dataMovie}
+      />
+    </div>
+  );
+}
+function StreamLayout({ dataEpisode, dataSeason, dataMovie }) {
+  const video = `https://image.tmdb.org/t/p/w500/${dataEpisode.still_path}`;
+  const title = dataEpisode.name;
+  const date = dataEpisode.air_date;
+  const age = dataMovie.content_ratings.results[2].rating;
+  const poster_season = dataSeason.poster_path;
+  const poster = `https://media.themoviedb.org/t/p/w300_and_h450_face/${poster_season}`;
+  const genres = dataMovie.genres
+    .map((g) => {
+      return g.name;
+    })
+    .join(", ");
 
+  const [comments, setComments] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [loveCount, setLoveCount] = useState(0);
+  const handleSendComment = () => {
+    if (inputText === "") return;
+
+    const newComment = {
+      id: Date.now,
+      text: inputText,
+      date: new Date().toLocaleDateString("id-ID"),
+    };
+
+    setComments([newComment, ...comments]);
+    setInputText("");
+  };
+  const handleCancelComment = () => {
+    setInputText("");
+  };
+
+  const episodeCards = dataSeason.episodes.map((e) => {
+    const poster_episode = `https://image.tmdb.org/t/p/w500/${e.still_path}`;
+    const name_episode = e.episode_number;
+    const cardURL = `/stream/${e.show_id}/${e.season_number}/${e.episode_number}`;
+
+    return (
+      <Link to={cardURL} key={name_episode} className="episode-cards">
+        <div className="episode-img">
+          <img src={poster_episode} alt="" />
+        </div>
+        <div className="episode-title">
+          <h4>Episode {name_episode}</h4>
+        </div>
+      </Link>
+    );
+  });
+
+  return (
+    <>
+      <section className="watch-player">
+        <div className="video-frame">
+          <img src={video} alt="" />
+        </div>
+        <div className="video-detail">
+          <div className="video-title">
+            <section className="video-name">{title}</section>
+            <div className="video-etc">
+              <div className="video-age">17+</div>
+              <div className="video-year">{date}</div>
+              <div className="video-number">
+                Episode {dataEpisode.episode_number}
+              </div>
+            </div>
+          </div>
+          <div className="serial-detail">
+            <h3>Serial Detail</h3>
+            <a
+              href="watch.html?id=${id}&season=${season}"
+              className="serial-poster"
+            >
+              <div className="serial-left">
+                <img src={poster} alt="" />
+              </div>
+              <div className="serial-right">
+                <div className="serial-name">
+                  {dataMovie.name} {dataSeason.name}
+                </div>
+                <div className="serial-etc">
+                  <div className="serial-age">{age}</div>
+                  <div className="serial-year">{dataSeason.air_date}</div>
+                </div>
+              </div>
+            </a>
+            <div className="serial-info">
+              <p>{dataEpisode.overview}</p>
+            </div>
+            <div className="genre-list">
+              <div class="serial-genre">{genres}</div>
+            </div>
+          </div>
+        </div>
+        <div className="video-comment">
+          <div className="comment-top">
+            <h1>
+              Comment's (<span>{comments.length}</span>)
+            </h1>
+          </div>
+          <div className="comment-user">
+            <div className="user-icon">
+              <i className="bi bi-person-circle"></i>
+            </div>
+            <div className="input-comment">
+              <input
+                type="text"
+                className="input-box"
+                placeholder="Add comment's"
+                value={inputText}
+                onChange={(e) => setInputText(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSendComment()}
+              />
+              <div className="input-wrapper">
+                <button className="cancel-button" onClick={handleCancelComment}>
+                  Cancel
+                </button>
+                <button className="send-button" onClick={handleSendComment}>
+                  Send
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="comment-list">
+            {comments.map((item) => {
+              return (
+                <>
+                  <div class="comment-card">
+                    <div class="comment-icon">
+                      <i class="bi bi-person-circle"></i>
+                    </div>
+                    <div class="comment-info">
+                      <div class="comment-name">Homudoka {item.date}</div>
+                      <div class="user-input">{item.text}</div>
+                      <div class="comment-reply">
+                        <div class="icon-love">
+                          <i
+                            class="bi bi-heart"
+                            onClick={() => setLoveCount(loveCount + 1)}
+                          ></i>{" "}
+                          {loveCount} like
+                        </div>
+
+                        <div class="path">
+                          <i class="bi bi-flag"></i>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <hr />
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+      <aside className="episode-panel">
+        <div className="episode-list">
+          <a
+            href="watch.html?id=${id}&season=${season}"
+            className="all-episode"
+          >
+            All Episode
+          </a>
+          {episodeCards}
+        </div>
+      </aside>
+    </>
+  );
+}
 function App() {
   return (
     <BrowserRouter>
@@ -936,7 +1121,10 @@ function App() {
           path="/season/:seasonNumber/:id"
           element={<SeasonDetail />}
         ></Route>
-        <Route path="/stream/:id/:seasonNumber/:episodenumber"></Route>
+        <Route
+          path="/stream/:id/:seasonNumber/:episodeNumber"
+          element={<StreamDetail />}
+        ></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
